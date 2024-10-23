0c879e2060a75e3c2d52a4688628bb1f3034363d
HsiaoHungKai <hsiaohungkai@gmail.com>
2024-10-23 11:55:04 +0800

    Add TLButton and refactor HeaderView

---
+++ ToDoList/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  ToDoList
+//
+//  Created by Earnest Hsiao on 2024/10/23.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
